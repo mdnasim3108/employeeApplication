dc8e4adb059ca30327db2756e94b9556a63efbae
nishaanth <nishaanth20.nn@gmail.com>
2024-02-16 13:39:18 +0530

    cm4

--- src/services/EmployeeService.java
+++ src/services/EmployeeService.java
@@ -16,11 +16,13 @@ public class EmployeeService {
 	Employee e2=new Employee(2, "nishanth", 2000, "onebill", "CIT");
 	
 	employeeAddress ea1=new employeeAddress(e1, 10, "kallai", "vilanthangal street", "india", "tamilnadu", "606202");
-	employeeAddress ea2Address=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
+	employeeAddress ea2=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
 
 	LinkedList<employeeAddress> employeeInfosWithAdress=new LinkedList<>();
 	
 	public void addEmployeeWithAdress(employeeAddress ea) {
+		employeeInfosWithAdress.add(ea1);
+		employeeInfosWithAdress.add(ea2);
 		employeeInfosWithAdress.add(ea);
 	}
 
