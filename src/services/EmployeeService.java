2052ab15a69d10e9ddf049af61fce285225b25e9
md nasim <mohamednasim3108@gmail.com>
2024-02-16 13:35:15 +0530

    empServicesortandview

--- src/services/EmployeeService.java
+++ src/services/EmployeeService.java
@@ -16,17 +16,51 @@ public class EmployeeService {
 	Employee e2=new Employee(2, "nishanth", 2000, "onebill", "CIT");
 	
 	employeeAddress ea1=new employeeAddress(e1, 10, "kallai", "vilanthangal street", "india", "tamilnadu", "606202");
-	employeeAddress ea2Address=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
+	employeeAddress ea2=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
 
 	LinkedList<employeeAddress> employeeInfosWithAdress=new LinkedList<>();
 	
-	public void addEmployeeWithAdress(employeeAddress ea) {
-		employeeInfosWithAdress.add(ea);
-	}
 
-	public void showEmployees(){
+	
+
+	
+
+	
+	public  void addEmployeeWithAdress(employeeAddress ea) {
+		employeeInfosWithAdress.add(ea);
+		
+	}
+	
+	
+
+	public  void showEmployees(){
 		for(employeeAddress emp:employeeInfosWithAdress) 
 		System.out.println(emp.toString());
+	}
+	
+	
+	
+	public boolean checkIfEmployeeExist(int id) {
+		for(employeeAddress ea: employeeInfosWithAdress) {
+			if(id==ea.getId()) return true;
+		}
+		return false;
+	}
+	
+	public employeeAddress viewEmployee(int id) {
+			boolean flag=checkIfEmployeeExist(id);
+			if(flag) {
+				for(employeeAddress ea:employeeInfosWithAdress) {
+					if(id==ea.getId()) return ea;
+				}
+			}
+			return null;
+	}
+	
+	public void sortEmployeeById() {
+		Comparator<employeeAddress> sortById=Comparator.comparingInt(employeeAddress::getId);
+		Collections.sort(employeeInfosWithAdress,sortById);
+		showEmployees();
 	}
 
 	
