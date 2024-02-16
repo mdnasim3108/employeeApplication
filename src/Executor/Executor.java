fe4c7e02da348cbe8f08a9ef3e69c5a3bcb9bc39
nishaanth <nishaanth20.nn@gmail.com>
2024-02-16 14:30:55 +0530

    empUpdate

--- src/Executor/Executor.java
+++ src/Executor/Executor.java
@@ -15,12 +15,20 @@ public class Executor {
     
     String flag=null;
     
+    
+    
     EmployeeService emp=new EmployeeService();
     UserServices user= new UserServices();
     
     Scanner input=new Scanner(System.in);
+    Employee e1=new Employee(1, "nasim", 1000, "onebill", "GCT");
+	Employee e2=new Employee(2, "nishanth", 2000, "onebill", "CIT");
+	
+	employeeAddress ea1=new employeeAddress(e1, 10, "kallai", "vilanthangal street", "india", "tamilnadu", "606202");
+	employeeAddress ea2=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
     
-    
+    emp.employeeInfosWithAdress.add(ea2);
+    emp.employeeInfosWithAdress.add(ea1);
     do{
         
 
@@ -43,11 +51,11 @@ public class Executor {
 	    System.out.println("5)Sort Employee");
 	    int option=Integer.parseInt(input.nextLine());
 	    
-	    if(option==1 || option==2  ) {
-	        if(!selectedAdmin.getRole().equals("admin_all"))
-	            System.out.println("cannot access these option");
-	    
-	    	else{
+//	    if(option==1 || option==2  ) {
+//	        if(!selectedAdmin.getRole().equals("admin_all"))
+//	            System.out.println("cannot access these option");
+//	    }
+//	    	else{
 	
 	        switch(option){
 	            case 1:
@@ -104,18 +112,51 @@ public class Executor {
 	        	    System.out.println("1)Salary");
 	        	    System.out.println("2)Age");
 	        	    System.out.println("3)Cureent company");
-	        	    System.out.println("4) company");
+	        	    System.out.println("4) prev company");
 	                
-//	        	    int option=Integer.parseInt(input.nextLine());
-//	        	    switch()
+	        	    int opt=Integer.parseInt(input.nextLine());
+	        	    switch(opt) {
+	        	    case 1:
+	        	    	System.out.println("Enter Employee Salary");
+		                int eSalary=Integer.parseInt(input.nextLine());
+		                Employee employee1 = emp.viewEmployee(id);
+		                employee1.setSalary(eSalary);
+		                break;
+		            
+	        	    case 2:
+	        	    	System.out.println("Enter Employee age");
+		                int eage=Integer.parseInt(input.nextLine());
+		                Employee employee2 = emp.viewEmployee(id);
+		                employee2.setSalary(eage);
+		                break;    
+	        	    
+		                
+	        	    case 3:
+	        	    	System.out.println("Enter Employee current company");
+		                String eCurCompany=input.nextLine();
+		                Employee employee3 = emp.viewEmployee(id);
+		                employee3.setCurrCompany(eCurCompany);
+		                break;
+	        	    
+	        	    
+	            	case 4:
+        	    	System.out.println("Enter Employee prev company");
+	                String eprevCompany=input.nextLine();
+	                Employee employee4 = emp.viewEmployee(id);
+	                employee4.setCurrCompany(eprevCompany);
+	                break;
+	                
+	                default:
+	                	System.out.println("invalid option");
+        	    }
 	                
 	                break;
 	
 	            case 3:
 	            	System.out.println("ether the employee id");
-	            	String empname=input.nextLine();
-	            	
-	            	
+	            	int empId=Integer.parseInt(input.nextLine());
+	            	employeeAddress ea=emp.viewEmployee(empId);
+	            	System.out.println(ea.toString());
 	            	
 	                break;
 	
@@ -126,7 +167,7 @@ public class Executor {
 	                break;
 	            
 	        	case 5:
-	        		
+	        		emp.sortEmployeeById();
 	        		break;
 	        		
 	        	
@@ -134,8 +175,8 @@ public class Executor {
 	        		System.out.println("Do you want to continue (y/n)");
 			        flag= input.nextLine();
 			        
-        	}
-	    }
+        	//}
+	    
     	}while(flag.equals("y") );
     	
     	input.close();
