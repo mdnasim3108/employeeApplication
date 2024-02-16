2ac060220cb7c8ee4dd55001e38fe9ae74ec2014
md nasim <mohamednasim3108@gmail.com>
2024-02-16 12:46:26 +0530

    employeeService

--- src/services/EmployeeService.java
+++ src/services/EmployeeService.java
@@ -1,21 +1,37 @@
 package services;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
+
 
 import Admin.Admin;
+import Employee.Employee;
+import Employee.employeeAddress;
 
 
 
 public class EmployeeService {
-	
-	
-	
-	public void addEmployee(int id,String name,String salary,int age,String currentCompany,String prevCompany) {
 
 
-		
+	Employee e1=new Employee(1, "nasim", 1000, "onebill", "GCT");
+	Employee e2=new Employee(2, "nishanth", 2000, "onebill", "CIT");
+	
+	employeeAddress ea1=new employeeAddress(e1, 10, "kallai", "vilanthangal street", "india", "tamilnadu", "606202");
+	employeeAddress ea2Address=new employeeAddress(e2, 20, "covai", "sample street", "india", "tamilnadu", "606202");
+
+	LinkedList<employeeAddress> employeeInfosWithAdress=new LinkedList<>();
+	
+	public void addEmployeeWithAdress(employeeAddress ea) {
+		employeeInfosWithAdress.add(ea);
 	}
+
+	public void showEmployees(){
+		for(employeeAddress emp:employeeInfosWithAdress) 
+		System.out.println(emp.toString());
+	}
+
+	
+
+
 
 	
 
